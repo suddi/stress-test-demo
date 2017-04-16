2b5c832c11126707e037864404b83566c66e35fc
Sudharshan Ravindran <suddir@gmail.com>
2017-04-16 13:06:49 +0800

    Corrected indentation

--- scripts/tester.js
+++ scripts/tester.js
@@ -3,48 +3,48 @@
 const request = require('request');
 
 function validateResponse(headers, body) {
-	if (headers.statusCode !== 200) {
-		return false;
-	}
+    if (headers.statusCode !== 200) {
+        return false;
+    }
 
-	if (body.status !== 'OK' || !body.hash) {
-		return false;
-	}
+    if (body.status !== 'OK' || !body.hash) {
+        return false;
+    }
 
-	console.log(headers.statusCode + ': ' + body.hash);
-	return true;
+    console.log(headers.statusCode + ': ' + body.hash);
+    return true;
 }
 
 function makeRequest(params, callback) {
-	request(params, function (err, headers, body) {
-		if (err) {
-			return callback(err);
-		} else if (!validateResponse(headers, JSON.parse(body))) {
-			return callback(new Error('Failed validation'));
-		}
-		return callback(null, true);
-	});
+    request(params, function (err, headers, body) {
+        if (err) {
+            return callback(err);
+        } else if (!validateResponse(headers, JSON.parse(body))) {
+            return callback(new Error('Failed validation'));
+        }
+        return callback(null, true);
+    });
 }
 
 function run() {
-	const params = {
-		url: 'http://localhost:3002/testing-words',
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	};
+    const params = {
+        url: 'http://localhost:3002/testing-words',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
 
-	let error;
-	for (let i = 0; i < 10000; i++) {
-		makeRequest(params, function (err, pass) {
-			error = err;
-		});
-	}
-	console.log(error);
-	// console.log(error.stack);
+    let error;
+    for (let i = 0; i < 10000; i++) {
+        makeRequest(params, function (err, pass) {
+            error = err;
+        });
+    }
+    console.log(error);
+    // console.log(error.stack);
 }
 
 if (!module.parent) {
-	run();
+    run();
 }
